138b19e82a80c8eb7d5359eb8dcf44fc98eab8df
shwetha <shwetha12206@gmail.com>
2026-08-11 19:30:09 +0530

    solution

---
+++ 500. Keyboard Row/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findWords(self, words):
+        row1 = set("qwertyuiop")
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
+
+        result = []
+
+        for word in words:
+            letters = set(word.lower())
+        
+            if letters <= row1 or letters <= row2 or letters <= row3:
+                
+
+                result.append(word)
+
+        return result
