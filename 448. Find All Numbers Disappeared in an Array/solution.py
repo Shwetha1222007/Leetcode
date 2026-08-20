c0f962ef5753a05bf0a6adcf91543309deb59e6d
shwetha <shwetha12206@gmail.com>
2026-08-20 20:00:41 +0530

    solution

---
+++ 448. Find All Numbers Disappeared in an Array/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        result = []
+
+        n = len(nums)
+
+        for i in range(1,n+1):
+
+            if i not in nums:
+                result.append(i)
+        return result
+        
